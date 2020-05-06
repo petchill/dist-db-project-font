f27c2ce5c72d0c3027613df7e06060c63b08e950
songpondev <songpon111333@gmail.com>
2020-05-06 13:17:26 +0700

    edit sever

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,6 +1,3 @@
 let SERVER_URL = "http://localhost:3300"
-if (process.env.REACT_APP_ENV==='development'){
-  SERVER_URL = 'https://ad609692.ngrok.io'
-}
 process.env.SEVER_URL = SERVER_URL;
 export { SERVER_URL };
